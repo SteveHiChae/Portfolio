87f0f78253a4d7fe2e8bdff9d0548df074cbb867
steve <stvchae@gmail.com>
2016-02-12 21:39:02 +0900

    My first web portfolio

---
+++ js/index.js
@@ -0,0 +1,7 @@
+
+$('#main_navbar > ul.navbar-nav li').click(function(e) {
+  $('.nav li.active').removeClass('active');
+  var $this = $(this);
+  $this.addClass('active');
+  // e.preventDefault();
+});
